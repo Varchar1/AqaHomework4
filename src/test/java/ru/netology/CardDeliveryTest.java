808d8162bbebaaeca7f14b3ab0173a124a15c5a3
QA <test@neto.com>
2023-05-25 21:39:44 +0300

    Task doneWithBadge revision

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -22,13 +23,17 @@ public class CardDeliveryTest {
     public void shouldInterfaceTest() {
         open("http://localhost:9999/");
         Configuration.holdBrowserOpen = true;
+        String planingDate = generateDate(4,"dd.MM.yyyy");
         $("[data-test-id='city'] input").setValue("Пенза");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME),Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(generateDate(4, "dd.MM.yyyy"));
+        $("[data-test-id='date'] input").setValue(planingDate);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79990009999");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planingDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
